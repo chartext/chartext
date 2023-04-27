a1a3b5ccb67e66c3d15cfeb6ac0c517774641a00
threaddot <threaddot@users.noreply.github.com>
2023-04-27 15:23:20 -0400

    Fixes type ordering

--- packages/chart/src/axis/Axis.types.ts
+++ packages/chart/src/axis/Axis.types.ts
@@ -4,14 +4,14 @@ export type AxisType = 'x' | 'y';
 
 export type AxisPosition = 'top' | 'right' | 'bottom' | 'left';
 
-export type AxisProps = {
-  size: number;
-  showZero: boolean;
-  theme: AxisTheme;
-};
-
 export type AxisTheme = {
   tickColor: string;
   zeroTickColor: string;
   fontSize: number;
+};
+
+export type AxisProps = {
+  size: number;
+  showZero: boolean;
+  theme: AxisTheme;
 };
